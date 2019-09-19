90a6f04c513301738e5c1789b7ef8ca953770ebb
jesusgarce <tarraga.jesus@gmail.com>
2019-09-19 12:25:46 +0200

    Add FractionTest #9

---
+++ src/test/java/es/upm/miw/iwvg/ecosystem/miscellaneous/FractionTest.java
@@ -0,0 +1,45 @@
+package es.upm.miw.iwvg.ecosystem.miscellaneous;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FractionTest {
+    Fraction fraction;
+
+    @BeforeEach
+    void createFraction(){
+        fraction = new Fraction(6,3);
+    }
+
+    @Test
+    void testFractionIntInt(){
+        fraction = new Fraction(6,2);
+        assertEquals(6, fraction.getNumerator());
+        assertEquals(2, fraction.getDenominator());
+    }
+
+    @Test
+    void testFraction(){
+        fraction = new Fraction();
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+
+    @Test
+    void testDecimal(){
+        assertEquals(2,fraction.decimal());
+    }
+
+    @Test
+    void testEnumerator(){
+        assertEquals(3, fraction.getDenominator());
+    }
+
+    @Test
+    void testNumerator(){
+        assertEquals(6,fraction.getNumerator());
+    }
+}
+
